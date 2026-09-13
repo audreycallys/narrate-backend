44cb16da2c4a0175b9872863c91b7539a1a084bb
audreycallys <nevaely0107@gmail.com>
2026-09-13 11:32:32 +0700

    feat: tambahkan post update delete routes kategori

--- src/routes/categories/categories.route.ts
+++ src/routes/categories/categories.route.ts
@@ -9,4 +9,10 @@ router.get("/", CategoriesController.getCategories);
 // Get Category By ID
 router.get("/:id", CategoriesController.getCategoryById);
 
+// Update Category
+router.put("/:id", CategoriesController.updateCategory);
+
+// Delete Category
+router.delete("/:id", CategoriesController.deleteCategory);
+
 export default router;
