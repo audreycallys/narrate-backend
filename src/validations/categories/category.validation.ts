ea748ef35b28b99e9a53a1ce9f33b6b84f941612
audreycallys <nevaely0107@gmail.com>
2026-09-13 11:53:44 +0700

    refactor: menjadikan category sebagai data tetap

--- src/validations/categories/category.validation.ts
+++ src/validations/categories/category.validation.ts
@@ -1,25 +1,5 @@
 import { z } from "zod";
 
-export const createCategorySchema = z.object({
-  name: z
-    .string()
-    .min(2, "Nama category minimal 2 karakter")
-    .max(100, "Nama category maksimal 100 karakter"),
-
-  description: z.string().optional(),
-});
-
 export const categoryIdSchema = z.object({
   id: z.coerce.number().int().positive("Category ID tidak valid"),
-});
-
-export const updateCategorySchema = z.object({
-  name: z
-    .string()
-    .min(2, "Nama category minimal 2 karakter")
-    .max(100, "Nama category maksimal 100 karakter"),
-
-  description: z
-    .string()
-    .optional(),
 });
