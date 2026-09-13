6899e6b4742fc0c23b9d5ed52d7726eaa8361377
audreycallys <nevaely0107@gmail.com>
2026-09-13 11:49:56 +0700

    refactor: hapus post update delete controller kategori

--- src/controllers/categories/categories.controller.ts
+++ src/controllers/categories/categories.controller.ts
@@ -2,11 +2,7 @@ import { Request, Response } from "express";
 import { db } from "../../config/db";
 import { categoriesTable } from "../../config/schema";
 import { asc, eq } from "drizzle-orm";
-import {
-  createCategorySchema,
-  categoryIdSchema,
-  updateCategorySchema,
-} from "../../validations/categories/category.validation";
+import { categoryIdSchema } from "../../validations/categories/category.validation";
 
 export class CategoriesController {
   // Membaca Semua Category
@@ -26,39 +22,6 @@ export class CategoriesController {
       });
     } catch (error) {
       console.error("Get categories error:", error);
-
-      return res.status(500).json({
-        success: false,
-        message: "Terjadi kesalahan pada server",
-        error: error instanceof Error ? error.message : error,
-      });
-    }
-  };
-
-  // Membuat Category
-  createCategory = async (req: Request, res: Response) => {
-    try {
-      const validateData = createCategorySchema.parse(req.body);
-
-      const { name, description } = validateData;
-
-      const [newCategory] = await db
-        .insert(categoriesTable)
-        .values({
-          name,
-          description,
-        })
-        .returning();
-
-      return res.status(201).json({
-        success: true,
-        message: "Category created successfully",
-        data: {
-          category: newCategory,
-        },
-      });
-    } catch (error) {
-      console.error("Create category error:", error);
 
       return res.status(500).json({
         success: false,
@@ -95,89 +58,6 @@ export class CategoriesController {
       });
     } catch (error) {
       console.error("Get category by id error:", error);
-
-      return res.status(500).json({
-        success: false,
-        message: "Terjadi kesalahan pada server",
-        error: error instanceof Error ? error.message : error,
-      });
-    }
-  };
-
-  // Update Category
-  updateCategory = async (req: Request, res: Response) => {
-    try {
-      const validatedParams = categoryIdSchema.parse(req.params);
-      const { id } = validatedParams;
-
-      const validateData = updateCategorySchema.parse(req.body);
-      const { name, description } = validateData;
-
-      const [existingCategory] = await db
-        .select()
-        .from(categoriesTable)
-        .where(eq(categoriesTable.id, id));
-
-      if (!existingCategory) {
-        return res.status(404).json({
-          success: false,
-          message: "Category Not Found",
-        });
-      }
-
-      const [updatedCategory] = await db
-        .update(categoriesTable)
-        .set({
-          name,
-          description,
-        })
-        .where(eq(categoriesTable.id, id))
-        .returning();
-
-      return res.status(200).json({
-        success: true,
-        message: "Category updated successfully",
-        data: {
-          category: updatedCategory,
-        },
-      });
-    } catch (error) {
-      console.error("Update category error:", error);
-
-      return res.status(500).json({
-        success: false,
-        message: "Terjadi kesalahan pada server",
-        error: error instanceof Error ? error.message : error,
-      });
-    }
-  };
-
-  // Delete Category
-  deleteCategory = async (req: Request, res: Response) => {
-    try {
-      const validatedParams = categoryIdSchema.parse(req.params);
-      const { id } = validatedParams;
-
-      const [existingCategory] = await db
-        .select()
-        .from(categoriesTable)
-        .where(eq(categoriesTable.id, id));
-
-      if (!existingCategory) {
-        return res.status(404).json({
-          success: false,
-          message: "Category Not Found",
-        });
-      }
-
-      await db.delete(categoriesTable).where(eq(categoriesTable.id, id));
-
-      return res.status(200).json({
-        success: true,
-        message: "Category deleted successfully",
-      });
-    } catch (error) {
-      console.error("Delete category error:", error);
 
       return res.status(500).json({
         success: false,
